c2b872c4aa3241db1eb85fd92f38851c1a75caae
Himanshu Verma <145604241+Hverma1206@users.noreply.github.com>
2024-10-20 00:12:56 +0530

    homepage

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,24 +1,56 @@
-import React from 'react'
-import {
-  Form,
-  Input,
-  Button,
-  Typography
+import React from 'react';
+import './Home.css';
 
-} from 'antd'
+import { Form, Input, Button, Typography } from 'antd';
 
-const {Title, Text,Link} = Typography;
+const { Title, Text } = Typography;
+
 const Home = () => {
-  const onFinish =(values) =>{
-    console.log('Form Values:', values)
-  }
+  const onFinish = (values) => {
+    console.log('Form Values:', values);
+  };
+
   return (
-    <div classname='container'>
-      <div classname='form-wrapper'>
-        <Title level ={3} style={{color:'#fff'}}></Title>
+    <div className='cont'>
+      <div className='form'>
+        <Title level={3} style={{ color: '#FFF' }}>LumosHub</Title>
+        <Text className='room-text'>Enter your Room ID</Text>
+        <div style={{marginTop:'20px'}}>
+        <Form
+          name='join_room'
+          onFinish={onFinish}
+          layout='vertical'
+          className='Form'
+        >
+          <Form.Item 
+            name="RoomID"
+            rules={[{ required: true, message: 'Please Input Your Room ID!' }]}
+          >
+            <Input placeholder='Room ID' className='custom-input'/>
+          </Form.Item>
+          <Form.Item
+            name='username'
+            rules={[{ required: true, message: 'Please Input Your Username!' }]}
+          >
+            <Input placeholder='Username' className='custom-input'/>
+          </Form.Item>
+          <Form.Item 
+          >
+            <Button type='primary' htmlType='submit' block className='custom-btn'> 
+              Join
+            </Button>
+          </Form.Item>
+        </Form>
         </div>
-       
+        <div className='spacing'>
+        <Text className='not-id'> Don't have a Room ID? </Text>
+        <Button className='create-room-btn' block href='#'>
+          Create a Room
+        </Button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
+
 export default Home;
