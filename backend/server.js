a627396c9843d3cb0096cc9221be3ac70f87a18c
Himanshu Verma <145604241+Hverma1206@users.noreply.github.com>
2024-10-27 22:54:28 +0530

    minor change

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const {Server} = require('socket.io');
+const express = require('express')
+const {Server} = require('socket.io')
 const app = express()
-const http = require('http');
+const http = require('http')
 const server = http.createServer(app)
 const io = new Server(server)
 
