802e2bc577ed170825d0b908038cad7fe4f26b76
Himanshu Verma <145604241+Hverma1206@users.noreply.github.com>
2024-10-29 00:53:49 +0530

    socket

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,20 @@ const http = require('http')
 const server = http.createServer(app)
 const io = new Server(server)
 
+const userSocketMap = {}
+const getAllCOnnectedClients = (roomId) =>{
+  return io.sockets.adapter.rooms.get(roomId)
+}
+
+
 io.on("connection" , (socket) => {
-  console.log(`User connected : ${socket.id}`)
+  // console.log(`User connected : ${socket.id}`)
+
+  socket.on('join', ({roomId, username}) =>{
+   userSocketMap[socket.id] = username
+   socket.join(roomId)
+   const clients = getAllCOnnectedClients(roomId)
+  })
 })
 const PORT = process.env.PORT || 5000
 server.listen(PORT, ()=> console.log("Server is running"))
