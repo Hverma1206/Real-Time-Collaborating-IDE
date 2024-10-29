580197613617d13991b25062e07b49eb944b6181
Himanshu Verma <145604241+Hverma1206@users.noreply.github.com>
2024-10-30 00:03:46 +0530

    server

--- backend/server.js
+++ backend/server.js
@@ -7,17 +7,32 @@ const io = new Server(server)
 
 const userSocketMap = {}
 const getAllCOnnectedClients = (roomId) =>{
-  return io.sockets.adapter.rooms.get(roomId)
+  return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map(
+    (socketId) => {
+      return {
+        socketId,
+        username: userSocketMap[socketId],
+      }
+    }
+  )
 }
 
 
 io.on("connection" , (socket) => {
-  // console.log(`User connected : ${socket.id}`)
 
   socket.on('join', ({roomId, username}) =>{
    userSocketMap[socket.id] = username
    socket.join(roomId)
    const clients = getAllCOnnectedClients(roomId)
+   clients.forEach(({socketId})=>{
+    io.to(socketId).emit('joined'),{
+      clients,
+      username,
+      socketId :socket.id
+    }
+
+   }
+  )    
   })
 })
 const PORT = process.env.PORT || 5000
