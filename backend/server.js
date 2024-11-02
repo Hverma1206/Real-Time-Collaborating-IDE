17c3fad9ebb2413a1033f4e83774dd47f92cbf6b
Himanshu Verma <145604241+Hverma1206@users.noreply.github.com>
2024-11-02 21:58:15 +0530

    leave sucess toast failed

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,68 @@
-git
+const express = require('express');
+const { Server } = require('socket.io');
+const http = require('http');
+const { disconnect } = require('process');
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+const userSocketMap = {};
+
+const getAllConnectedClients = (roomId) => {
+  return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map((socketId) => {
+    return {
+      socketId,
+      username: userSocketMap[socketId],
+    };
+  });
+};
+io.on('connection', (socket) => {
+  socket.on('join', ({ roomId, username }) => {
+    // Check if the room exists
+    const roomExists = io.sockets.adapter.rooms.has(roomId);
+
+    if (!roomExists) {
+      // If the room does not exist, emit an error to the client
+      socket.emit('room_not_found', 'Room does not exist.');
+      return;
+    }
+
+    // If the room exists, proceed to join
+    userSocketMap[socket.id] = username;
+    socket.join(roomId);
+    const clients = getAllConnectedClients(roomId);
+
+    clients.forEach(({ socketId }) => {
+      io.to(socketId).emit('joined', {
+        clients,
+        username,
+        socketId: socket.id,
+      });
+    });
+  });
+
+  socket.on('leave', ({ roomId, username }) => {
+    socket.leave(roomId);
+    const clients = getAllConnectedClients(roomId);
+
+    clients.forEach(({ socketId }) => {
+      io.to(socketId).emit('left', { username });
+    });
+
+    delete userSocketMap[socket.id];
+  });
+
+  socket.on('disconnecting', () => {
+    const rooms = [...socket.rooms];
+    rooms.forEach((roomId) => {
+      socket.in(roomId).emit('disconnected', {
+        socketId: socket.id,
+        username: userSocketMap[socket.id],
+      });
+    });
+    delete userSocketMap[socket.id];
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
