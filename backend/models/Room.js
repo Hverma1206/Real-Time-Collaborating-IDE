980b978d4982068d6a63c327b7a1f7038357f696
Himanshu Verma <145604241+Hverma1206@users.noreply.github.com>
2024-11-08 14:28:44 +0530

    not define error fixed

--- backend/models/Room.js
+++ backend/models/Room.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const roomSchema = new mongoose.Schema({
-    roomId: {type: string , required : true},
+    roomId: {type: String , required : true},
     users: [{type :mongoose.Schema.Types.ObjectId, ref :'User'}]
 })
 const Room = mongoose.model('Room', roomSchema)
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,11 +1,23 @@
-const mongoose = require('mongoose')
-const { Socket } = require('socket.io')
+const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username : {type :  String, required: true},
-    SocketId : {type: String, required: true},
-    role: {type: String, required : true},
-    roomID: {type : String, required: true},
-})
-const User = mongoose.model('User', schema)
-module.exports = User 
+  username: {
+    type: String,
+    required: true,
+  },
+  roomId: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ['admin', 'writer', 'reader'],
+    default: 'reader',
+  },
+  socketId: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('User', userSchema);
